f747476f2170ffe2eb9c9b4a59c964956b423c26
EduardPr <eduardprov@yandex.ru>
2019-07-18 17:16:09 +0600

    Factory Tests

---
+++ OOPCalcultor.Tests/OneArgumentCalculator/OneArgumentsFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OOPCalculator.OneArgumentCalculator;
+using OOPCalculator.TwoArgumentsCalculator;
+
+namespace OOPCalcultor.Tests.OneArgumentCalculator
+{
+    public class OneArgumentsFactoryTests
+    {
+        //IsInstanceOf:
+
+        [TestCase("Arccos", typeof(ArccosCalculator))]
+        [TestCase("Arcsin", typeof(ArcsinCalculator))]
+        [TestCase("Arctan", typeof(ArctanCalculator))]
+        [TestCase("Cos", typeof(CosinusCalculator))]
+        [TestCase("Ctan", typeof(CtanCalculator))]
+        [TestCase("Expow", typeof(EPowerX))]
+        [TestCase("Ln", typeof(LnCalculator))]
+        [TestCase("Log10", typeof(Log10Calculator))]
+        [TestCase("Log2", typeof(Log2Calculator))]
+        [TestCase("Sin", typeof(SinusCalculator))]
+        [TestCase("Tan", typeof(TanCalculator))]
+        [TestCase("Pow", typeof(XPowTwoCalculator))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
---
+++ OOPCalcultor.Tests/TwoArgumentsCalculator/TwoArgumentsFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OOPCalculator.OneArgumentCalculator;
+using OOPCalculator.TwoArgumentsCalculator;
+
+namespace OOPCalcultor.Tests.TwoArgumentsCalculator
+{
+    public class TwoArgumentsFactoryTests
+    {
+        [TestCase("Add", typeof(AdditionCalculator))]
+        [TestCase("Arith", typeof(ArithmeticMeanCalculator))]
+        [TestCase("Division", typeof(DivisionCalculator))]
+        [TestCase("Geom", typeof(GeometricMeanCalculator))]
+        [TestCase("LogX", typeof(LogX))]
+        [TestCase("Max", typeof(MaxCalculator))]
+        [TestCase("Min", typeof(MinCalculator))]
+        [TestCase("Multiplication", typeof(MultiplyCalculator))]
+        [TestCase("PointDegree", typeof(PointDegreeCalculator))]
+        [TestCase("RadiusVector", typeof(PointRadiusVectorCalculator))]
+        [TestCase("Remainder", typeof(RemainderDivisionCalculator))]
+        [TestCase("Subtraction", typeof(SubtractionCalculator))]
+        [TestCase("XPow1DivisionY", typeof(XPow1DivisionYCalculator))]
+        [TestCase("XPowY", typeof(XPowYCalculator))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
